4b2aecf3742400a572fe29716d9fb1dba17d7072
sp0ngi3 <studnia12321@yahoo.com>
2023-05-14 19:01:06 +0200

    Added the router with pages

--- olafportfolio/src/App.js
+++ olafportfolio/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Experience from "./pages/Experience";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element=<Home /> />
+          <Route path="/experience" element=<Experience /> />
+          <Route path="/portfolio" element=<Portfolio /> />
+          <Route path="/contact" element=<Contact /> />
+          <Route path="/about" element=<About /> />
+        </Routes>
+      </Router>
     </div>
   );
 }
